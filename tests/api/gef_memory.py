ea60f6c5f63ff0c67ffe699bf6156501d9ef247f
xcr <xcr@xcr>
2026-06-23 21:57:47 +0800

    Kusanagi238__diff__id_35

--- tests/api/gef_memory.py
+++ tests/api/gef_memory.py
@@ -4,15 +4,15 @@
 
 import pathlib
 import random
+
 import pytest
 
 from tests.base import RemoteGefUnitTestGeneric
-
 from tests.utils import (
+    GDBSERVER_DEFAULT_HOST,
     debug_target,
     gdbserver_session,
     qemuuser_session,
-    GDBSERVER_DEFAULT_HOST,
 )
 
 
@@ -33,7 +33,7 @@ class GefMemoryApi(RemoteGefUnitTestGeneric):
         assert gef.memory.maps is not None
 
     def test_api_gef_memory_parse_info_proc_maps_expected_format(self):
-        if self.gdb_version < (11, 0):
+        if tuple(self.gdb_version) < (11, 0):
             pytest.skip(f"Skipping test for version {self.gdb_version} (min 10.0)")
 
         gdb, root = self._gdb, self._conn.root
@@ -68,7 +68,7 @@ class GefMemoryApi(RemoteGefUnitTestGeneric):
 
         Section = root.eval("Section")
 
-        if self.gdb_version < (11, 0):
+        if tuple(self.gdb_version) < (11, 0):
             # expect an exception
             with pytest.raises(AttributeError):
                 next(gef.memory.parse_gdb_info_proc_maps())
